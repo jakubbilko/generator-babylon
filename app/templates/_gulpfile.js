84058cc6a53c1b5e1ec7fd2754f04009c34db38a
Alex Jordan <alex@strugee.net>
2015-06-24 14:52:35 -0700

    Fix whitespace; fix small syntax problem

--- app/templates/_gulpfile.js
+++ app/templates/_gulpfile.js
@@ -1,17 +1,17 @@
-var gulp = require('gulp');
-wiredep = require('wiredep').stream,
-browserSync = require('browser-sync'),
-reload = browserSync.reload,
-useref = require('gulp-useref'),
-gulpif = require('gulp-if'),
-uglify = require('gulp-uglify'),
-minifyCss = require('gulp-minify-css'),
-merge = require('merge-stream');
+var gulp = require('gulp'),
+    wiredep = require('wiredep').stream,
+    browserSync = require('browser-sync'),
+    reload = browserSync.reload,
+    useref = require('gulp-useref'),
+    gulpif = require('gulp-if'),
+    uglify = require('gulp-uglify'),
+    minifyCss = require('gulp-minify-css'),
+    merge = require('merge-stream');
 
 gulp.task('wiredep', function() {
-	gulp.src('./app/index.html').
-	pipe(wiredep()).
-	pipe(gulp.dest('./app'));
+	gulp.src('./app/index.html')
+	    .pipe(wiredep())
+	    .pipe(gulp.dest('./app'));
 });
 
 gulp.task('serve', function() {
@@ -21,8 +21,8 @@ gulp.task('serve', function() {
 		server: {
 			baseDir: 'app',
 			routes: {
-		    	'/bower_components': 'bower_components'
-		    }
+				'/bower_components': 'bower_components'
+			}
 		}
 	});
 	gulp.watch(['app/*.html', 'app/scripts/*.js', 'app/css/*.css']).on('change', reload);
@@ -37,16 +37,16 @@ gulp.task('clean', require('del').bind(null, ['dist']));
 
 gulp.task('dist', ['clean'], function() {
 	var assets = useref.assets();
-	
-	var stream1 = gulp.src('app/*.html').
-	pipe(assets).
-	pipe(gulpif('*.js', uglify())).
-    pipe(gulpif('*.css', minifyCss())).
-	pipe(assets.restore()).
-	pipe(useref()).
-	pipe(gulp.dest('dist'));
-	
+
+	var stream1 = gulp.src('app/*.html')
+	                  .pipe(assets)
+	                  .pipe(gulpif('*.js', uglify()))
+	                  .pipe(gulpif('*.css', minifyCss()))
+	                  .pipe(assets.restore())
+	                  .pipe(useref())
+	                  .pipe(gulp.dest('dist'));
+
 	var stream2 = gulp.src('app/assets/**/*').pipe(gulp.dest('dist/assets/'));
-	
+
 	return merge(stream1, stream2);
 });
