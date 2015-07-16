81fcada6f0343ae1eb4273b898ab136f377112a4
Alex Jordan <alex@strugee.net>
2015-07-15 22:02:47 -0700

    Don't expect bower.json in tests

--- test/test-app.js
+++ test/test-app.js
@@ -18,7 +18,6 @@ describe('babylon:app', function () {
 
   it('creates files', function () {
     assert.file([
-      'bower.json',
       'package.json',
       '.editorconfig',
       '.jshintrc'
